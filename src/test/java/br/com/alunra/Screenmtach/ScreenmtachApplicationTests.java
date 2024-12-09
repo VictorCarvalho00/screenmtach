098026559233b29022ec99b8077166f4d5dc9645
Victor GABRIEL ALVES CARVALHO XAVIER <e008732@rede.tst>
2024-12-09 16:32:53 -0300

    Inicio do porjeto Spring

---
+++ src/test/java/br/com/alunra/Screenmtach/ScreenmtachApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alunra.Screenmtach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmtachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
